03e2b4ee4fd3c45de1a765d45cf22280df6f01d0
Anstwer <stepankotov15101985@gmail.com>
2026-03-10 17:49:23 +1000

    Update daily.py

--- services/daily.py
+++ services/daily.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta
 from aiogram import Bot
 import logging
 
-from db.database import get_homework_for_date
+from db.database import get_full_homework_with_weekly
 from utils.formatter import format_homework_message
 
 logger = logging.getLogger(__name__)
@@ -11,7 +11,7 @@ async def send_daily_homework(bot: Bot, chat_id: int):
     """Отправляет ДЗ на завтра в указанный чат"""
     target_date = date.today() + timedelta(days=1)  # завтра
     try:
-        homework = await get_homework_for_date(target_date)
+        homework = await get_full_homework_with_weekly(target_date)
         text = format_homework_message(target_date, homework)
         await bot.send_message(chat_id, text, disable_web_page_preview=True)
         logger.info(f"Рассылка на {target_date} отправлена в чат {chat_id}")
@@ -21,4 +21,5 @@ async def send_daily_homework(bot: Bot, chat_id: int):
         try:
             await bot.send_message(chat_id, f"❌ Не удалось отправить ДЗ на завтра. Ошибка: {e}")
         except:
+
             pass
